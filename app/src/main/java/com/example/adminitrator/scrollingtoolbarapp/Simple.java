84e2bb1f9873b5acb5416847842c5c0c49690201
Pranesh <pranesh.msc@gmail.com>
2017-02-13 15:46:50 +0530

    Simple commited

---
+++ app/src/main/java/com/example/adminitrator/scrollingtoolbarapp/Simple.java
@@ -0,0 +1,12 @@
+package com.example.adminitrator.scrollingtoolbarapp;
+
+/**
+ * Created by Adminitrator on 2/13/2017.
+ */
+
+public class Simple {
+    public static void main()
+    {
+
+    }
+}
